2fe0ab6a893d9dc9a736a3691f22ea29e5ebdad4
shuye <80120075+wuzhi01@users.noreply.github.com>
2021-03-25 00:42:12 +0800

    移除过期

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -24,8 +24,6 @@
 1 3,13 * * * node /scripts/jd_oneplus.js >> /scripts/logs/jd_oneplus.log 2>&1
 #jd_live_lottery_social
 0 7-23/1 * * * node /scripts/jd_live_lottery_social.js >> /scripts/logs/jd_live_lottery_social.log 2>&1
-#jd_city
-0 */4 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
