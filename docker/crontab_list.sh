f235e38d167e2c14873e9cf80fc869b2239c3cf5
wuzhi01 <80120075+wuzhi01@users.noreply.github.com>
2021-03-27 00:19:33 +0800

    移除过期

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,8 +6,6 @@
 0 8 22-31/1 3 * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 京小兑
 30 9,15 * * * node /scripts/jd_jxd.js >> /scripts/logs/jd_jxd.log 2>&1
-# 极速版春香节红包
-30 9 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 # 超级直播间红包雨
 30,31 20-23/1 23 3 * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
 #京东国际盲盒活动时间】2021年02月23日 起至 2021年03月31日 18:00:00
